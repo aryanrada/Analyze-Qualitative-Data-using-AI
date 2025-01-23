8d084e610e7385858cf0ce47422c7beb20a05c39
AryanRadadiya200570960 <aryanradadiya18@gmail.com>
2025-01-23 09:54:52 -0500

    main

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import streamlit as st
+import pandas as pd
 
 st.sidebar.title("Analyze Qualitative Data using AI")
 
 uploaded_file = st.sidebar.file_uploader("Choose a file")
 if uploaded_file is not None:
-    bytes_data = uploaded_file.getvalue()
-    data = bytes_data.decode("utf-8")
+    data = pd.read_csv(uploaded_file)
+    #bytes_data = uploaded_file.getvalue()
+    #data = bytes_data.decode("utf-8")
+    #data = pd.DataFrame(data)
+    data = data.drop(columns=['Unnamed: 0', 'is_Original', 'Flair', 'URL'])
     st.write(data)
