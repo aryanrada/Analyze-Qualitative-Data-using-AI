feca242cbf8ff8411e79d688bc6df6d894c599a3
Aryan Radadiya <aryanradadiya18@gmail.com>
2025-02-12 20:03:34 -0500

    main

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 from bertopic import BERTopic
+import spacy
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 import organizer
 
@@ -19,14 +20,6 @@ def sentiment_analyzer_scores(text):
         Sentiment = 'Neutral'
     return Sentiment
 
-
-def bertopic(data):
-    topic_model = BERTopic()
-    topics, probabilities = topic_model.fit_transform(data['Title'])
-    data['Title_Topic'] = topics
-    data['Title_Probability'] = probabilities
-    return data
-
 uploaded_file = st.sidebar.file_uploader("Choose a file")
 if uploaded_file is not None:
     data = pd.read_csv(uploaded_file)
@@ -40,10 +33,11 @@ if uploaded_file is not None:
     subreddit.insert(0,"Overall")
     option = st.sidebar.selectbox("Select Subreddit", subreddit)
 
-    model = st.sidebar.radio("Select Model", ("VADAR", "BERTopic"))
+    model = st.sidebar.radio("Select Model", ("VADAR", "BERTopic", "Spacy NER"))
 
     if st.sidebar.button("Show Analysis"):
         data = organizer.fetch_data(option,data)
+
         if model == 'VADAR':
             data['Title_Sentiment'] = data['Title'].apply(sentiment_analyzer_scores)
 
@@ -65,8 +59,51 @@ if uploaded_file is not None:
             sentiment_counts_df = sentiment_counts.reset_index()
             sentiment_counts_df.columns.name = None
             st.write(sentiment_counts_df)
-        else:
+
+        elif model == "BERTopic":
             #data = organizer.bertopic(data)
             st.title("BERTopic")
+            topic_model = BERTopic()
+            topics, probabilities = topic_model.fit_transform(data["Title"])
+            data['Title_Topic'] = topics
+            data['Title_Probability'] = probabilities
+            topic_data = topic_model.get_topic_info()
+            topic_hierarchy = topic_model.visualize_hierarchy()
+            topic_visual = topic_model.visualize_topics()
+            st.header("Analyzed data")
+            st.write(topic_data)
+            st.header("Visual Representation")
+            st.write(topic_visual)
+            st.write(topic_hierarchy)
+
+        elif model == "Spacy NER":
+            model = spacy.load("en_core_web_sm")
+
+            def ner(text):
+                doc = model(text)
+                entities = []
+                for ent in doc.ents:
+                    entities.append((ent.text, ent.label_))
+                return entities
+            
+            data['entities'] = data['Title'].apply(ner)
+
+            ner_data = []
+            for index, row in data.iterrows():
+                text = row['Title']
+                for entity, entity_type in row['entities']:
+                    ner_data.append([text, entity, entity_type])
+
+            ner_data = pd.DataFrame(ner_data, columns=['text', 'entity', 'entity_type'])
+
+            st.title("Spacy NER")
+            st.header("Analyzed data")
+            st.write(ner_data)
+
+            st.header("Graphical Representation")
+            entity_count = ner_data["entity_type"].value_counts()
+            fig, ax = plt.subplots()
+            ax.bar(entity_count)
+            st.pyplot(fig)
 
         
