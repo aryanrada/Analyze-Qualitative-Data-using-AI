a6449fa7228d22063d8ab068400b57e02caebb6d
Aryan Radadiya <aryanradadiya18@gmail.com>
2025-03-20 16:47:32 -0400

    main

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
+from wordcloud import WordCloud
 import torch
 from io import BytesIO
 import organizer
 import algorithms
 
+st.set_page_config(layout="wide")
+#torch.hub.list('pytorch/vision', force_reload=True)
 torch.classes.__path__ = []
 #hi
 st.sidebar.title("Analyze Qualitative Data using AI")
@@ -39,51 +42,60 @@ if uploaded_file is not None:
             #data = organizer.fetch_data(option,data)
 
             if model == 'VADAR':
+                st.title("Sentiment Analysis")
                 #data['Title_Sentiment'] = data['Title'].apply(algorithms.sentiment_analyzer)
                 data['Text_Sentiment'] = data['text1'].apply(algorithms.sentiment_analyzer)
-                st.title("Sentiment Analysis")
-            
-                st.header("Overall Sentiment Analysis")
-                fig, ax = plt.subplots()
-                ax.pie(data['Text_Sentiment'].value_counts(), labels = data['Text_Sentiment'].value_counts().index, autopct='%1.1f%%')
-                st.pyplot(fig)
+                col1, col2 = st.columns(2)
+                with col1:
+                    st.header("Overall Sentiment Analysis")
+                    fig, ax = plt.subplots()
+                    ax.pie(data['Text_Sentiment'].value_counts(), labels = data['Text_Sentiment'].value_counts().index, autopct='%1.1f%%')
+                    st.pyplot(fig)
 
-                buf = BytesIO()
-                fig.savefig(buf, format="png")
-                buf.seek(0)
+                    #buf = BytesIO()
+                    #fig.savefig(buf, format="png")
+                    #buf.seek(0)
 
-                with open("chart.png", "wb") as f:
-                    f.write(buf.getvalue())
+                    #with open("chart.png", "wb") as f:
+                    #    f.write(buf.getvalue())
 
-                with open("chart.png", "rb") as file:
-                    st.download_button(
-                        label="Download Chart",
-                        data=file,
-                        file_name="chart.png",
-                        mime="image/png",
-                    )
-                
+                    #with open("chart.png", "rb") as file:
+                    #    st.download_button(
+                    #        label="Download Chart",
+                    #        data=fig,
+                    #        file_name="chart.png",
+                    #        mime="image/png",
+                    #    )
+                with col2:
+                    st.header("Word Cloud")
+                    text = " ".join(data['text1'].astype(str))
+                    wordcloud = WordCloud(width=800, height=800, colormap='binary').generate(text)
+                    fig, ax = plt.subplots()
+                    ax.imshow(wordcloud, interpolation='bilinear')
+                    ax.axis('off')
+                    st.pyplot(fig)
                 #st.header("Sentiment Analysis by Subreddit")
                 #fig, ax = plt.subplots()
                 #ax = sns.countplot(x='Subreddit', hue='Title_Sentiment', data=data)
                 #ax = sns.countplot(hue='Title_Sentiment', data=data)
                 #plt.xticks(rotation=90)
                 #st.pyplot(fig)
+                col1, col2, col3 = st.columns([2, 3, 2])
+                with col2:
+                    st.header("Sentiment counts by Subreddit")
+                    #sentiment_counts = data['Text_Sentiment'].groupby(data['Subreddit']).value_counts().unstack().fillna(0)
+                    sentiment_counts = data
+                    sentiment_counts_df = sentiment_counts.reset_index()
+                    sentiment_counts_df.columns.name = None
+                    st.write(sentiment_counts_df)
 
-                st.header("Sentiment counts by Subreddit")
-                #sentiment_counts = data['Text_Sentiment'].groupby(data['Subreddit']).value_counts().unstack().fillna(0)
-                sentiment_counts = data
-                sentiment_counts_df = sentiment_counts.reset_index()
-                sentiment_counts_df.columns.name = None
-                st.write(sentiment_counts_df)
-
-                csv = organizer.convert_df(sentiment_counts_df)
-                st.download_button(
-                    label="Download data",
-                    data=csv,
-                    file_name="data.csv",
-                    mime="text/csv",
-                )
+                    csv = organizer.convert_df(sentiment_counts_df)
+                    st.download_button(
+                        label="Download data",
+                        data=csv,
+                        file_name="data.csv",
+                        mime="text/csv",
+                    )
 
             elif model == "BERTopic":
                 st.title("BERTopic")
@@ -91,19 +103,23 @@ if uploaded_file is not None:
                 topic_data = topic_model.get_topic_info()
                 topic_hierarchy = topic_model.visualize_hierarchy()
                 topic_visual = topic_model.visualize_topics()
-                st.header("Analyzed data")
-                st.write(topic_data)
+                col1, col2, col3 = st.columns([1, 4, 1])
+                with col2:
+                    st.header("Analyzed data")
+                    st.write(topic_data)
 
-                csv = organizer.convert_df(topic_data)
-                st.download_button(
-                    label="Download data",
-                    data=csv,
-                    file_name="data.csv",
-                    mime="text/csv",
-                )
+                    csv = organizer.convert_df(topic_data)
+                    st.download_button(
+                        label="Download data",
+                        data=csv,
+                        file_name="data.csv",
+                        mime="text/csv",
+                    )
 
-                st.header("Visual Representation")
-                st.write(topic_visual)
+                col1, col2 = st.columns(2)
+                with col1:
+                    st.header("Visual Representation")
+                    st.write(topic_visual)
 
                 # fig, ax = plt.subplots()
                 # topic_visual.write_image(ax)
@@ -121,25 +137,27 @@ if uploaded_file is not None:
                 #         file_name="chart.png",
                 #         mime="image/png",
                 #     )
-
-                st.header("Topic Hierarchy")
-                st.write(topic_hierarchy)
+                with col2:
+                    st.header("Topic Hierarchy")
+                    st.write(topic_hierarchy)
 
             elif model == "Spacy NER":
                 st.title("Spacy NER")
 
                 data = algorithms.spacy_ner(data)
+                
+                col1, col2, col3 = st.columns([2, 3, 2])
+                with col2:
+                    st.header("Analyzed data")
+                    st.write(data)
 
-                st.header("Analyzed data")
-                st.write(data)
-
-                csv = organizer.convert_df(data)
-                st.download_button(
-                    label="Download data",
-                    data=csv,
-                    file_name="data.csv",
-                    mime="text/csv",
-                )
+                    csv = organizer.convert_df(data)
+                    st.download_button(
+                        label="Download data",
+                        data=csv,
+                        file_name="data.csv",
+                        mime="text/csv",
+                    )
 
                 st.header("Graphical Representation")
                 entity_count = pd.DataFrame(data["entity_type"].value_counts())
@@ -149,20 +167,20 @@ if uploaded_file is not None:
                 plt.xticks(rotation=90)
                 st.pyplot(fig)
 
-                buf = BytesIO()
-                fig.savefig(buf, format="png")
-                buf.seek(0)
+                #buf = BytesIO()
+                #fig.savefig(buf, format="png")
+                #buf.seek(0)
 
-                with open("chart.png", "wb") as f:
-                    f.write(buf.getvalue())
+                #with open("chart.png", "wb") as f:
+                #    f.write(buf.getvalue())
 
-                with open("chart.png", "rb") as file:
-                    st.download_button(
-                        label="Download Chart",
-                        data=file,
-                        file_name="chart.png",
-                        mime="image/png",
-                    )
+                #with open("chart.png", "rb") as file:
+                #    st.download_button(
+                #        label="Download Chart",
+                #       data=file,
+                #        file_name="chart.png",
+                #        mime="image/png",
+                #    )
     except Exception as e:
         st.sidebar.error(f"Error: {e}")
 
--- organizer.py
+++ organizer.py
@@ -4,6 +4,9 @@ from docx import Document
 import pandas as pd
 from nltk.tokenize import sent_tokenize
 import streamlit as st
+import unicodedata
+import nltk
+from nltk.corpus import stopwords
 
 #def fetch_data(subreddit,df):
 #    if subreddit != 'Overall':
@@ -78,12 +81,73 @@ def filter_text_columns(df):
 def clean_dataset(df):
     #df = remove_url_columns(df)
     df = filter_text_columns(df)
-    
-    return df
-
     #url_pattern = re.compile(r'https?://\S+|www\.\S+')
 
     #def is_url_column(series):
     #    return series.astype(str).str.contains(url_pattern, na=False).any()
     
     #df = df.loc[:, ~df.apply(is_url_column)]
+    return df
+
+def basic_clean(string):
+    '''
+    This function takes in a string and
+    returns the string normalized.
+    '''
+    string = unicodedata.normalize('NFKD', string)\
+             .encode('ascii', 'ignore')\
+             .decode('utf-8', 'ignore')
+    string = re.sub(r'[^\w\s]', '', string).lower()
+    return string
+
+def tokenize(string):
+    '''
+    This function takes in a string and
+    returns a tokenized string.
+    '''
+    tokenizer = nltk.tokenize.ToktokTokenizer()
+    string = tokenizer.tokenize(string, return_str = True)
+
+    return string
+
+def stem(string):
+    '''
+    This function takes in a string and
+    returns a string with words stemmed.
+    '''
+    ps = nltk.porter.PorterStemmer()
+    stems = [ps.stem(word) for word in string.split()]
+    string = ' '.join(stems)
+    
+    return string
+
+def lemmatize(string):
+    '''
+    This function takes in string for and
+    returns a string with words lemmatized.
+    '''
+    wnl = nltk.stem.WordNetLemmatizer()
+    lemmas = [wnl.lemmatize(word) for word in string.split()]
+    string = ' '.join(lemmas)
+    
+    return string
+
+def remove_stopwords(string, extra_words = [], exclude_words = []):
+    '''
+    This function takes in a string, optional extra_words and exclude_words parameters
+    with default empty lists and returns a string.
+    '''
+    stopword_list = stopwords.words('english')
+    stopword_list = set(stopword_list) - set(exclude_words)
+    stopword_list = stopword_list.union(set(extra_words))
+    words = string.split()
+    filtered_words = [word for word in words if word not in stopword_list]
+    string_without_stopwords = ' '.join(filtered_words)
+    
+    return string_without_stopwords
+
+def clean(text):
+    '''
+    This function combines the above steps and added extra stop words to clean text
+    '''
+    return remove_stopwords(lemmatize(basic_clean(text)))
